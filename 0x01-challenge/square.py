b16b41f0765a9296e19df1369af2797ec91f4491
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-03-10 12:17:02 +0100

    Fix 1 for task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+""" Module for Class Square
+"""
 
-class square():
-    
+
+class Square():
+    """ Definition of class square
+    """
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """ Initialize the class with 2 variables """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,15 +19,17 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiterofmysquare(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String representation """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiterofmysquare())
